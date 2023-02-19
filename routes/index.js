7843902aed4474111a452f30a980757207a92ad5
Assane Ali Boye <aalijr97@gmail.com>
2023-02-19 22:43:43 +0000

    ekee

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(' jenkins node app');
+  res.send('Bienvenue dan jenkins-node-app');
 });
 
 module.exports = router;
